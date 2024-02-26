935de534dd38f67b03a696de45b52c2ce5dfed00
Evanilson Pereira <pevanilson99@gmail.com>
2024-02-26 12:43:32 -0300

    Pulling data from API

--- src/routes/news.js
+++ src/routes/news.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const newsRouter = express.Router();
+const axios = require('axios');
 
-newsRouter.get('/', (req, res) => {
-    res.render('partials/news');
+newsRouter.get('/', async (req, res) => {
+
+    try {
+        const newsAPI = await axios.get(`https://raddy.dev/wp-json/wp/v2/posts/`);
+        res.render('partials/news', { articles: newsAPI.data });
+    } 
+
+    catch (err) {
+        if(err.response) {
+            console.log(err.response.data);
+            console.log(err.response.status);
+            console.log(err.response.headers);
+
+        } else if (err.request) {
+            console.log(err.request);
+
+        } else {
+            console.error('Error', err.message);
+        }
+    }
 });
 
 module.exports = newsRouter;
