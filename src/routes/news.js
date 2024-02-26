68e559bfcf5284bac77e09ac03795a84132c01f3
Evanilson Pereira <pevanilson99@gmail.com>
2024-02-26 13:12:12 -0300

    Pulling more data from API

--- src/routes/news.js
+++ src/routes/news.js
@@ -11,14 +11,18 @@ newsRouter.get('/', async (req, res) => {
 
     catch (err) {
         if(err.response) {
+            // In case we mispell the API link, we still want to render the page with an error message
+            res.render('partials/news', { articles: null });
             console.log(err.response.data);
             console.log(err.response.status);
             console.log(err.response.headers);
 
         } else if (err.request) {
+            res.render('partials/news', { articles: null }); // In case we mispell the API link, we still want to render the page with an error message
             console.log(err.request);
 
         } else {
+            res.render('partials/news', { articles: null }); // In case we mispell the API link, we still want to render the page with an error message
             console.error('Error', err.message);
         }
     }
